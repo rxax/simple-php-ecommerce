c1aa2b4bd0b1309e5d3278fca37ee9085ea50419
george <rszgeorge@gmail.com>
2026-08-21 12:53:11 +0300

    legal notice and cookie-consent-settings

---
+++ src/Controller/StaticContentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class StaticContentController extends AbstractController
+{
+    #[Route('/legal', name: "legal")]
+    public function login(): Response
+    {
+        return $this->render('legal/notice.html.twig');
+    }
+}
